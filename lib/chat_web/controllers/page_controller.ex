60f9f62bbe905f455aedafc1434e4205a1c28606
Travis Young <t.w.young.ubc@gmail.com>
2019-01-27 16:06:10 -0500

    initial commit

---
+++ lib/chat_web/controllers/page_controller.ex
@@ -0,0 +1,9 @@
+defmodule ChatWeb.PageController do
+  use ChatWeb, :controller
+  alias Chat.Chats
+
+  def index(conn, _params) do
+    messages = Chats.list_messages
+    render(conn, "index.html", messages: messages)
+  end
+end
